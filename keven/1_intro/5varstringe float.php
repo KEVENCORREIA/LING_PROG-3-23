c7f6cd980d7dbaf2baf809ef1c7770dba4b34308
KEVENCORREIA <125514343+KEVENCORREIA@users.noreply.github.com>
2023-03-01 19:42:31 -0300

    Add files via upload

---
+++ keven/1_intro/5varstringe float.php
@@ -0,0 +1,27 @@
+<?php
+$name = "kevenrafael";
+$age = 16;
+echo $name;
+echo"<br>";
+echo $age;
+echo"<hr>";
+$b = true;
+echo gettype($b);
+echo "<hr>";
+
+$nasc = 2006;
+ echo gettype($nasc);
+ 
+ 
+ 
+  echo "variaveis";
+
+ $float = 10.00;
+ echo float($float);
+ 
+ echo "variaveis";
+
+ $float = 'lalala123';
+ echo float($string);
+ 
+  ?>
---
+++ keven/1_intro/variaveis.01.03.php
@@ -0,0 +1,31 @@
+<?php
+
+$idade = 17; 
+
+ if ($idade < 18){
+	 echo 'você não pode entrar aqui! <hr>';
+ }else { 
+ echo 'seja bem - vindo <hr>';
+ }
+
+
+
+$idade = 17; 
+
+ if ($idade >= 18){
+	 echo 'você é maior de idade! <hr>';
+ }else { 
+ echo 'você é menor de idade <hr>';
+ }
+
+
+$idade = 17; 
+
+ if ($idade > 20){
+	 echo 'você está liberado! <hr>';
+ }else { 
+ echo 'você não pode entrar neste ambiente <hr>';
+ }
+
+?>
+  
